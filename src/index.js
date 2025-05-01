ca304251383df2d9ab0fbdcb57e80196203b71e2
kdev307 <127479436+kdev307@users.noreply.github.com>
2025-05-01 17:24:11 +0530

    Implemented useEffect and did error handling

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom/client';
-import StarRating from './StarRating';
-// import './index.css';
-// import App from './App';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import StarRating from "./StarRating";
+import "./index.css";
+import App from "./App";
 
-function Test(){
-  const [movieRating, setMovieRating] = useState(0);
+// function Test(){
+//   const [movieRating, setMovieRating] = useState(0);
 
-  return (
-    <div>
-      <StarRating color='#00f'maxRating={10} onSetRating={setMovieRating}/>
-      <p>This movie was rated {movieRating} stars</p>
-    </div>
-  )
-}
+//   return (
+//     <div>
+//       <StarRating color='#00f'maxRating={10} onSetRating={setMovieRating}/>
+//       <p>This movie was rated {movieRating} stars</p>
+//     </div>
+//   )
+// }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    {/* <App /> */}
-    <StarRating maxRating={10} messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}/>
+    <React.StrictMode>
+        <App />
+        {/* <StarRating maxRating={10} messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}/>
     <StarRating color='#f00' size={24} defaultRating={3}/>
-    <Test/>
-  </React.StrictMode>
+    <Test/> */}
+    </React.StrictMode>
 );
